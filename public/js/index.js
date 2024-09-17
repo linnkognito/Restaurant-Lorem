366dd04255fa40190159a8a60011bbbfd116e8d8
Linn Johansen <linn.es.johansen@gmail.com>
2024-09-17 10:00:44 -0700

    Working on slideshow

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,8 @@ const about = document.querySelector('#about');
 const menu = document.querySelector('#menu');
 const contact = document.querySelector('#contact');
 
+const picturesContainer = document.querySelector('.pictures-container');
+const pictures = document.querySelectorAll('.slideshow-img');
 const dotsContainer = document.querySelector('.dots-container');
 const dots = document.querySelectorAll('.dot');
 
@@ -46,59 +48,102 @@ const slideshow = [
     {
       path: '../public/img/photos/picture-0.jpg',
       alt: 'Dessert',
-      class: 'slideshow-img pictures-img-1',
+      class: 'slideshow-img',
     },
     {
       path: '../public/img/photos/picture-1.jpg',
       alt: 'Greenery and creek',
-      class: 'slideshow-img pictures-img-1',
+      class: 'slideshow-img',
     },
     {
       path: '../public/img/photos/picture-2.jpg',
       alt: 'Close up of a plate with foie gras dumplings for fall',
-      class: 'slideshow-img pictures-img-1',
+      class: 'slideshow-img',
     },
   ],
   [
     {
-      path: '../public/img/photos/picture-3.jpg',
+      path: '../img/photos/picture-3.jpg',
       alt: 'Flower',
-      class: 'slideshow-img pictures-img-1',
+      class: 'slideshow-img',
     },
     {
-      path: '../public/img/photos/picture-4.jpg',
+      path: '../img/photos/picture-4.jpg',
       alt: 'Close up of a plate with foie gras dumplings for spring',
-      class: 'slideshow-img pictures-img-1',
+      class: 'slideshow-img',
     },
     {
-      path: '../public/img/photos/picture-5.jpg',
+      path: '../img/photos/picture-5.jpg',
       alt: 'Japanese garden',
-      class: 'slideshow-img pictures-img-1',
+      class: 'slideshow-img',
     },
   ],
 ];
-// markup = `
-//     <img
-//           src="${img.path}"
-//           alt="${img.alt}"
-//           class="${img.class}"
-//         />
-//     `;
 
 dotsContainer.addEventListener('click', (e) => {
   if (!e.target.classList.contains('dot-icon')) return;
-
-  const dot = e.target;
-  const dotsArr = [...dots];
-  let activeDot = dotsArr.find((el) => el.classList.contains('active'));
+  if (e.target.closest('li').classList.contains('active')) return;
 
   const getIndex = (el) => el.getAttribute('data-index');
 
-  console.log(activeDot);
-  console.log(slideshow[getIndex(activeDot)]);
+  const dot = e.target.closest('li');
+  const index = getIndex(dot);
+  const dotsArr = [...dots];
+  let currentDot = dotsArr.find((el) => el.classList.contains('active'));
+  const picturesArray = [...pictures];
+  const currentIndex = getIndex(currentDot); // get active dot's index
 
-  // Apply minimize to active dots image array (nested)
+  const imgMarkup = (img, className) => {
+    return `
+    <img
+    src="${img.path}"
+    alt="${img.alt}"
+    class="${img.class} ${className}"
+    />
+    `;
+  };
 
-  // Apply minimize
-  //slideshow[index].forEach((img) => {});
+  const addAnimation = function (el, className, time) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        el.classList.add(className);
+        resolve();
+      }, time);
+    });
+  };
+
+  // Visually indicate what dot is active:
+  currentDot.classList.remove('active');
+  currentDot.querySelector('img').classList.remove('dot-active');
+  dot.classList.add('active');
+  dot.querySelector('img').classList.add('dot-active');
+
+  const applyAnimations = async function () {
+    // Minimize current images
+    for (const img of picturesArray) {
+      await addAnimation(img, 'minimize', 200);
+    }
+
+    // Clear
+    picturesContainer.innerHTML = '';
+    /*
+    // Maximize new images
+    for (const img of slideshow[index].flat()) {
+      picturesContainer.insertAdjacentHTML(
+        'afterbegin',
+        imgMarkup(img, 'scale-zero')
+      );
+
+      await addAnimation(img, 'maximize', 200);
+    }
+    */
+  };
+
+  applyAnimations();
 });
+
+//console.log('currentDot:', currentDot);
+//console.log('dot:', dot);
+//console.log('index:', index);
+//console.log('dotsArr:', dotsArr);
+//console.log('currentIndex:', currentIndex);
