d5889eca02a992734b6eaedb9c9576097dd778a7
Linn Johansen <linn.es.johansen@gmail.com>
2024-09-16 17:06:43 -0700

    Step 1 of slideshow done

--- public/js/index.js
+++ public/js/index.js
@@ -4,12 +4,11 @@ import { init } from './init';
 const nav = document.querySelector('nav');
 const home = document.querySelector('#home');
 const about = document.querySelector('#about');
-//const pictures = document.querySelector('#pictures');
 const menu = document.querySelector('#menu');
 const contact = document.querySelector('#contact');
 
-const images = document.querySelectorAll('.picture-container > img');
-const dots = document.querySelector('.pictures__dots');
+const dotsContainer = document.querySelector('.dots-container');
+const dots = document.querySelectorAll('.dot');
 
 //////////////////////////////////////////////
 
@@ -39,10 +38,67 @@ const navigation = function (e) {
 
 nav.addEventListener('click', (e) => navigation(e));
 
-dots.addEventListener('click', (e) => {
-  e.preventDefault();
-  let dot = e.target;
-  if (!dot.classList.contains('dot-icon')) return;
+//////////////////////////////////////////////
 
-  if (dot.classList('')) e.target.classList.add('minimize');
+// SLIDESHOW //
+const slideshow = [
+  [
+    {
+      path: '../public/img/photos/picture-0.jpg',
+      alt: 'Dessert',
+      class: 'slideshow-img pictures-img-1',
+    },
+    {
+      path: '../public/img/photos/picture-1.jpg',
+      alt: 'Greenery and creek',
+      class: 'slideshow-img pictures-img-1',
+    },
+    {
+      path: '../public/img/photos/picture-2.jpg',
+      alt: 'Close up of a plate with foie gras dumplings for fall',
+      class: 'slideshow-img pictures-img-1',
+    },
+  ],
+  [
+    {
+      path: '../public/img/photos/picture-3.jpg',
+      alt: 'Flower',
+      class: 'slideshow-img pictures-img-1',
+    },
+    {
+      path: '../public/img/photos/picture-4.jpg',
+      alt: 'Close up of a plate with foie gras dumplings for spring',
+      class: 'slideshow-img pictures-img-1',
+    },
+    {
+      path: '../public/img/photos/picture-5.jpg',
+      alt: 'Japanese garden',
+      class: 'slideshow-img pictures-img-1',
+    },
+  ],
+];
+// markup = `
+//     <img
+//           src="${img.path}"
+//           alt="${img.alt}"
+//           class="${img.class}"
+//         />
+//     `;
+
+dotsContainer.addEventListener('click', (e) => {
+  if (!e.target.classList.contains('dot-icon')) return;
+
+  const dot = e.target;
+  const dotsArr = [...dots];
+  let activeDot = dotsArr.find((el) => el.classList.contains('active'));
+
+  const getIndex = (el) => el.getAttribute('data-index');
+
+  console.log(activeDot);
+  console.log(slideshow[getIndex(activeDot)]);
+
+  // Apply minimize to active dots image array (nested)
+
+  // Apply minimize
+  //slideshow[index].forEach((img) => {});
 });
